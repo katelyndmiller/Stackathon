93bd6b5b76af616138c8bdee37740650996af663
Katelyn Miller <katdianemiller@gmail.com>
2022-10-18 14:11:41 -0400

    added isPrivate input to update form

--- client/components/UpdatePinPopupBox.js
+++ client/components/UpdatePinPopupBox.js
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 
 export default function UpdatePopup ({pin, updatePin, setUpdatePopupIsOpen}) {
     const [title, setTitle] = useState(pin.title);
-    const [description, setDescription] = useState(pin.description)
-    const [date, setDate] = useState(pin.date)
+    const [description, setDescription] = useState(pin.description);
+    const [date, setDate] = useState(pin.date);
+    const [isPrivate, setIsPrivate] = useState(pin.isPrivate);
   
     const handleSubmit = () => {
-      updatePin({...pin, title, description, date});
+      updatePin({...pin, title, description, date, isPrivate});
       setUpdatePopupIsOpen(false)
     }
   
@@ -17,6 +18,7 @@ export default function UpdatePopup ({pin, updatePin, setUpdatePopupIsOpen}) {
                     <input name="title" type="text" placeholder = 'Title' value={title} onChange={(e) => setTitle(e.target.value)}/>
                     <input name="date" type="text" placeholder = 'When did you visit?' value={date} onChange={(e) => setDate(e.target.value)}/>
                     <textarea placeholder = 'Description' name = 'description' value={description} onChange={(e) => setDescription(e.target.value)}/>
+                    <label>Private Pin <input name="private" type="checkbox" defaultChecked={pin.isPrivate} onChange={(e) => setIsPrivate(e.target.checked)} /></label>
                     <button type='submit'>Submit</button>
                 </form>
             </div>
